c9e795bddde318303739cf658dd140ac9b1de7e5
SamyuMT <jemycraft12@hotmail.com>
2024-12-20 13:48:11 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { TodoCount } from './TodoCount';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButtom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <TodoCount
-      completed={3}
-      total={4}/>
-
-      <TodoSearch/>
-      <TodoList>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-      </TodoList>
-      <iframe
-              src={"https://api.ventaflow.co/api/platform/session/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3ZlbnRhZmxvdy5jbyIsInN1YmRvbWFpbiI6ImplcmVtaW50aG11bm96c3dlYnNpdGUtMSIsImV4cCI6MTczNDcyMDAzMCwidXNlcklkIjo1OTQzODgsImNvbnRlbnQiOiI4eU5GVmdtMUlFN0ZZcG95In0.YSLGMbfX4_f82vE3DF1i37cbD0v98DGER-QqPy6jfNw"}
-            />
-      <CreateTodoButton/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Ruta embebida */}
+          <Route path="/embed" element={<EmbedPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+const EmbedPage = () => {
+  return (
+    <div>
+      <h1>Embed Page</h1>
+      {/* Aquí se embebe el iframe o contenido externo */}
+      <iframe
+        src="https://api.ventaflow.co/api/platform/session/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3ZlbnRhZmxvdy5jbyIsInN1YmRvbWFpbiI6ImplcmVtaW50aG11bm96c3dlYnNpdGUtMSIsImV4cCI6MTczNDcyMDk5MywidXNlcklkIjo1OTQzODgsImNvbnRlbnQiOiJkT3YzaG5mc2tzWnA0QmxjIn0.q5VgYdeuxq6jUYGpuNZ72uBmwNKqBPNbFDojiTtcZu4"
+        title="Website Embed"
+        width="100%"
+        height="500px"
+      />
+    </div>
+  );
+};
 
 export default App;
